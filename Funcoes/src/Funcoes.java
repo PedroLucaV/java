789d8aefafcc772db2529c33708cd9e5e5af5526
Pedro <izielucas2016@gmail.com>
2024-07-03 20:21:23 -0300

    function base learned!

--- Funcoes/src/Funcoes.java
+++ Funcoes/src/Funcoes.java
@@ -1,2 +1,10 @@
 public class Funcoes {
+    public static void main(String[] args){
+        Soma(1, 2);
+    }
+
+    public static void Soma(int x, int y){
+        int soma = x+y;
+        System.out.println(soma);
+    }
 }
